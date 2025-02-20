c820bdf47e456dcf379a392ca820855477e39b46
breadnut1906 <hmsbreadnut1906@gmail.com>
2025-02-20 09:15:38 +0800

    new system

---
+++ vcastplay-player/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    { 
+        path: '', 
+        loadComponent: () => import('./main-display/main-display.component').then((m) => m.MainDisplayComponent),
+        title: 'Main Display'
+    },
+    { path: "**", redirectTo: '/' },
+];
---
+++ vcastplay-player/src/app/main-display/main-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-display',
+  imports: [],
+  templateUrl: './main-display.component.html',
+  styleUrl: './main-display.component.scss'
+})
+export class MainDisplayComponent {
+
+}
