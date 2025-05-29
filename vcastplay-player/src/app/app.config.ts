59456a66763db956299749e621d8ceed93e54b3c
breadnut1906 <hmsbreadnut1906@gmail.com>
2025-05-29 10:35:22 +0800

    removed meta tag

--- vcastplay-player/src/app/app.config.ts
+++ vcastplay-player/src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
