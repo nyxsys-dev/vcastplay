841d0fe3e3e5adf60f0230eaf25a34a031a32cd9
breadnut1906 <hmsbreadnut1906@gmail.com>
2025-08-11 09:59:10 +0800

    updated path file

--- vcastplay-player/src/environments/environment.ts
+++ vcastplay-player/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     geoAPI: 'https://ipapi.co/json',
-    androidFilePath: 'https://appassets.androidplatform.net/medias',
+    androidFilePath: 'https://appassets.androidplatform.net/assets',
     version: '2.0.0',
     production: true,
 };
