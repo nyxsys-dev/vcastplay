72d99879f7a33deb5e158453e0607e67e02b22dc
breadnut1906 <hmsbreadnut1906@gmail.com>
2025-08-20 15:47:03 +0800

    add new image

--- vcastplay-player/src/app/core/services/player.service.ts
+++ vcastplay-player/src/app/core/services/player.service.ts
@@ -69,6 +69,13 @@ export class PlayerService {
         type: 'image',
         duration: 5
       },
+      {
+        id: 1,
+        name: 'photo-1464822759023-fed622ff2c3b.avif',
+        link: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'image',
+        duration: 10
+      }
     ]); 
   }
 
